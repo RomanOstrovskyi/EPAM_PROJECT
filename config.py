aa76462bd7c4e56e6b5cae1d791ce7ee0d707878
RomanOstrovskiy <romaostrovskiy616@gmail.com>
2023-02-28 16:52:59 +0200

    created models

---
+++ config.py
@@ -0,0 +1,8 @@
+USERNAME = 'root'
+PASSWORD = 'password'
+SERVER = 'localhost'
+DB = 'db'
+
+
+class Config:
+    SECRET_KEY = 'secret'
---
+++ connection.py
@@ -0,0 +1,8 @@
+from sqlalchemy import create_engine, MetaData
+from sqlalchemy.orm import Session
+from config import USERNAME, PASSWORD, SERVER, DB
+
+
+metadata = MetaData()
+engine = create_engine(f"mysql+pymysql://{USERNAME}:{PASSWORD}@{SERVER}/{DB}")
+session = Session(bind=engine)
