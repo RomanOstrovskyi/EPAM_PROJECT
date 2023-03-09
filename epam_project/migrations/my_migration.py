c0df61eee2b41163e8a6b82c8394efa5758d0e62
RomanOstrovskiy <romaostrovskiy616@gmail.com>
2023-03-09 19:28:57 +0200

    test commit 16

--- epam_project/migrations/my_migration.py
+++ epam_project/migrations/my_migration.py
@@ -1,7 +1,7 @@
 """ This file responsible for adding tables to database """
+from sqlalchemy import MetaData, Table, Column, Integer, String, ForeignKeyConstraint, Date
 import os
 import sys
-from sqlalchemy import MetaData, Table, Column, Integer, String, ForeignKeyConstraint, Date
 script_dir = os.path.dirname(os.path.abspath(__file__))
 connection_path = os.path.join(script_dir, '..', '..', 'connection.py')
 sys.path.append(os.path.dirname(connection_path))
